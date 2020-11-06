dd5700b38a1751cd107a2650fc86b805440440ed
Dohyung Ahn <me@rinae.dev>
2020-11-06 20:37:00 +0900

    remove unused files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc ideavimrc tmux.conf zshrc zpreztorc hyper.js purepower finicky.js"    # list of files/folders to symlink in homedir
+files="vimrc ideavimrc tmux.conf zshrc zpreztorc finicky.js"    # list of files/folders to symlink in homedir
 
 ##########
 
